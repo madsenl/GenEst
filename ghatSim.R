cdc014d1020011e033e0ab64d977a43fe89161e0
Lisa Madsen <lisa.madsen@oregonstate.edu>
2017-07-25 07:34:02 -0700

    Add files via upload

---
+++ ghatSim.R
@@ -0,0 +1,16 @@
+# Function to simulate nsims realizations of a vector of dependent ghats
+# with beta(a,b) marginals. Beta parameters a and b should have one element
+# for each class. Returns g_hat which is nsims by length(a).
+ghatSim <- function(nsims,a,b,rho) {
+  nclasses <- length(a)
+  Rho <- matrix(rho,nclasses,nclasses)
+  diag(Rho) <- 1
+  sqrtRho <- NA
+  try(sqrtRho <- chol(Rho), TRUE)
+  if (is.na(sqrtRho[1])) {
+    stop("Correlation matrix is not positive definite.")
+  }
+  U <- pnorm(matrix(rnorm(nclasses*nsims),nsims,nclasses)%*%sqrtRho) # Rho^(1/2) * standard normals transformed to uniform(0,1) so that cor(t(Z)) gives Rho approximately.
+  g_hat <- sapply(1:nclasses,FUN=function (x) qbeta(Z[x,],a[x],b[x])) 
+  return(g_hat)
+}
