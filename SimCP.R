2f4b739f51cffe27d19121fb09bac41079e87eb0
Lisa Madsen <lisa.madsen@oregonstate.edu>
2017-07-27 07:15:37 -0700

    Add files via upload

--- SimCP.R
+++ SimCP.R
@@ -10,8 +10,9 @@
 # is exponential, then the pda's are all 1's.
 #
 # A couple examples: The CP data set is from CPmod.R.
-# survOut <- survival::survreg(surv ~ spec+vis, dist = "exp", data = CP)
+# survOut <- survival::survreg(surv ~ spec*vis, dist = "exp", data = CP)
 # survOut <- survival::survreg(surv ~ spec*vis, dist = "weibull", data = CP)
+# survOut <- survival::survreg(surv ~ spec*vis, dist = "lognormal", data = CP)
 # 
 # egDat <- make_egDat(vars=all.vars(formula(survOut))[-1],dat=CP)
 # sims <- simCP(20,survOut,egDat)
@@ -19,24 +20,33 @@
 # sims$pdaSim
 
 simCP <- function(nsims,survOut,egDat) {
-  if (survOut$dist=="exponential") {
-    betahat <- survOut$coefficients # Exponential model's scale is 1.
-  } else {
-    betahat <- c(survOut$coefficients,log(survOut$scale)) 
-  }
-  betaSim <- mvtnorm::rmvnorm(nsims,mean=betahat,sigma=survOut$var)
   Xcp <- model.matrix(as.formula(paste("~",as.character(formula(survOut))[3],collapse=NULL)),
-                       data=egDat) # Extract formula and create model matrix.
-  # eoa parameters (pda, pdb) <-> survival (1/scale, exp(coef[i]))
-  pdbSim <- exp(betaSim[,1:NCOL(Xcp)]%*%t(Xcp))
+                      data=egDat) # Extract formula and create model matrix.
   if (survOut$dist=="exponential") {
-    pdaSim <- matrix(1,nrow=nsims,ncol=NROW(Xcp))
-  } else {   
-    pdaSim <- matrix(1/exp(betaSim[,NCOL(betaSim)]),nrow=nsims,ncol=NROW(Xcp))
-  }  
+    # Exponential model's scale is 1.
+    thetahat <- survOut$coefficients 
+  } else {
+    # For Weibull, loglogistic, and lognormal, the parameter vector consists of
+    # the coefficients of the predictor variables followed by log(scale).
+    thetahat <- c(survOut$coefficients,log(survOut$scale)) 
+  }
+  thetaSim <- mvtnorm::rmvnorm(nsims,mean=thetahat,sigma=survOut$var)
+  # Models and reparametrizations.
+  # exponential: a = 1; b = meanCP = exp(mod.e$coef)
+  # Weibull: a = shape = 1/mod.w$scale; b = scale = exp(mod.w$coef*Xcp)
+  # log-logistic: a = shape = 1/mod.ll$scale; b = scale = exp(mod.ll$*coef*Xcp)
+  # lognormal: a = sdlog^2 = mod.ln$scale^2; b = meanlog = mod.lm$coef*Xcp
+  pdaSim <- switch(survOut$dist,
+                   exponential=matrix(1,nrow=nsims,ncol=NROW(Xcp)),
+                   weibull=matrix(1/exp(thetaSim[,NCOL(thetaSim)]),nrow=nsims,ncol=NROW(Xcp)),
+                   loglogistic=matrix(1/exp(thetaSim[,NCOL(thetaSim)]),nrow=nsims,ncol=NROW(Xcp)),
+                   lognormal=matrix(1/thetaSim[,NCOL(thetaSim)]^2,nrow=nsims,ncol=NROW(Xcp)))
+  pdbSim <- switch(survOut$dist, 
+                   exponential=exp(thetaSim[,1:NCOL(Xcp)]%*%t(Xcp)),
+                   weibull=exp(thetaSim[,1:NCOL(Xcp)]%*%t(Xcp)),
+                   loglogistic=exp(thetaSim[,1:NCOL(Xcp)]%*%t(Xcp)),
+                   lognormal=thetaSim[,1:NCOL(Xcp)]%*%t(Xcp))
   colnames(pdbSim) <- egDat$cellNames
   colnames(pdaSim) <- egDat$cellNames
-  return(list(pdbSim=pdbSim,pdaSim=pdaSim))
+  return(list(pdaSim=pdaSim,pdbSim=pdbSim))
 }
-
-
